eebfdcd15d205d3d1a460d40446a8cfca4037116
prayogorizkymaulany <prayogo0907@gmail.com>
2017-11-08 00:06:58 +0800

    menampilkan tingg badan dan berat badan

--- 05.cpp
+++ 05.cpp
@@ -4,4 +4,11 @@ using namespace std;
 int main () {
 	float berat;
  	int tinggi;
+ 	
+cout << "Masukkan tinggi badan dalam cm : ";
+ cin >> tinggi;
+ 
+ cout << "Masukkan berat badan dalam kg : ";
+ cin >> berat; 
 }
+
